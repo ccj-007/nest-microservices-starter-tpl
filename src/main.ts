713d2c4b27d801fcbabe1ab5476b904c3c30dea1
ccj-007 <596487930@qq.com>
2023-08-05 23:45:14 +0800

    feat:tcp add

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      // options: {
+      //   host: 'http://localhost',
+      //   port: '3000',
+      // },
+    },
+  );
+  await app.listen();
 }
 
 bootstrap();
